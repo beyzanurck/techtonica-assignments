65bcb35008cf0c752ebb3304c72e821d48a4a912
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-07-27 15:01:00 -0500

    adds eventListener function to the button

--- recipe/script.js
+++ recipe/script.js
@@ -45,14 +45,18 @@ divComments.style.marginBottom = "20px"
 divOfOtherRecipe.appendChild(divComments)
 divComments.appendChild(buttonComments)
 
-//CREATE P TAGS FOR COMMENTS
-let comments = ["My daughter wanna eat it everyday!!", "Should I cook the sauce first?", "I love that!"]
+buttonComments.addEventListener("click", () => {
+    //CREATE P TAGS FOR COMMENTS
+    let comments = ["My daughter wanna eat it everyday!!", "Should I cook the sauce first?", "I love that!"]
 
-for(let i = 0; i < comments.length; i++){
-    const pTag = document.createElement("p")
-    pTag.textContent = comments[i]
-    divComments.appendChild(pTag)
-}
+    for(let i = 0; i < comments.length; i++){
+        const pTag = document.createElement("p")
+        pTag.textContent = comments[i]
+        divComments.appendChild(pTag)
+    }
+})
+
+
 
 
 
