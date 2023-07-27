bd3676b72755a08a63129a7c807026c000a3b170
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-07-27 08:54:00 -0500

    adds checkboxes to the Instructions

--- recipe/script.js
+++ recipe/script.js
@@ -1,1 +1,13 @@
-console.log("Test")
+const olOfInstructions = document.querySelector("ol")
+olOfInstructions.id = "instructions"
+
+const itemOfInstructions  = olOfInstructions.getElementsByTagName("li")
+for(let i = 0; i <  itemOfInstructions.length; i++){
+    const list = itemOfInstructions[i]
+
+    const checkbox = document.createElement("input")
+    checkbox.type ="checkbox"
+
+    list.prepend(checkbox)
+}
+
