67f8c6d1610069e8226cb12b472474d4ea236409
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-10 07:58:38 -0500

    adds images into img tags

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,5 +1,15 @@
+//GETS IMAGE PATH
 //browser does not allow to access file system.
-const picPath = []
+const path = []
 for (let index = 0; index < 8; index++) {
-    picPath.push(`/pics/${index+1}.jpg`)
+    path.push(`/pics/${index+1}.jpg`)
+}
+const picsPath = path.concat(path)
+
+//ADDS IMAGES INTO IMG TAGS
+const imgTags = document.getElementsByClassName("image")
+console.log(imgTags)
+
+for (let i = 0; i < imgTags.length; i++) {
+    imgTags[i].src = picsPath[i]
 }
